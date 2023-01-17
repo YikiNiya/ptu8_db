85fc81358a252fc3c17db0576c92c6a60470d8d3
YukiNiya <monikanemanyte17@gmail.com>
2023-01-17 08:27:37 +0200

    pakeista

--- darbuotojai/insert_darbuotojas.sql
+++ darbuotojai/insert_darbuotojas.sql
@@ -1,9 +1,3 @@
 -- SQLite
-<<<<<<< HEAD
 SELECT vardas, pavarde, atlyginimas
 FROM darbuotojai;
-=======
-INSERT INTO darbuotojai (vardas, pavarde, atlyginimas)
-VALUES ("Egle", "Smaguolyte", 3000);
-SELECT * FROM darbuotojai;
->>>>>>> origin/main
